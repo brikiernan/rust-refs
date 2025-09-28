7ced10ac8696638b0568d01000556f1594b68e12
Brian <brian@briankiernan.com>
2025-09-28 14:36:44 -0400

    complete course 🎉🔥🚀

--- learn-to-code-with-rust/clean_folders.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Loop through each directory in the current folder (one level deep)
-for dir in */; do
-  if [ -d "$dir" ]; then
-    echo "Cleaning $dir..."
-    (cd "$dir" && cargo clean)
-  fi
-done
